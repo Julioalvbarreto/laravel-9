16448dc470852fa4bc250d7b287828c8a096a0bd
julioalvbarreto <julioalvbarreto@gmail.com>
2022-04-01 17:28:22 -0300

    up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('users.index');
+    }
+
+    public function show($id)
+    {
+        echo '<h1>ID ' . $id . '</h1>';
     }
 }
